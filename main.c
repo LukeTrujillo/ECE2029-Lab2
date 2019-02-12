6890a47d671cd238ed1515af37f3353aec61b24f
Luke Trujillo <lrtrujillo@wpi.edu>
2019-02-11 19:33:44 -0500

    did a lot of stuff

--- main.c
+++ main.c
@@ -16,118 +16,183 @@ void swDelay(char numLoops);
 void configureButtons();
 unsigned int readButtons();
 
-void configUserLED(char inBits);
+void showLED(unsigned int);
+
+void configUserLED();
+void writeLED(unsigned int);
+void buzz(unsigned int);
+
+void makeSong();
 
 // Declare globals here
 unsigned int led = 0x00;
 
+struct Note
+{
+    int frequency, duration;
+};
+void playNote(struct Note not);
+
+struct Note song[10];
+
+volatile int nextBeat = 0;
+
 // Main
 void main()
 {
-    WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer. Always need to stop this!!
+   WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer. Always need to stop this!!
+   _BIS_SR(GIE);
 
-    initLeds();
+    configUserLED();
     configureButtons();
-    configDisplay();
-    configKeypad();
+    makeSong();
 
-    Graphics_clearDisplay(&g_sContext); // Clear the display
+    //setup the timer here
 
-    // Write some text to the display
-    Graphics_drawStringCentered(&g_sContext, "Welcome to", AUTO_STRING_LENGTH,
-                                48, 25, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "MSP430 HERO", AUTO_STRING_LENGTH,
-                                48, 35, TRANSPARENT_TEXT);
-    Graphics_drawStringCentered(&g_sContext, "Press * to start",
-    AUTO_STRING_LENGTH,
-                                48, 55, TRANSPARENT_TEXT);
-    Graphics_flushBuffer(&g_sContext);
+    int index = 0;
 
-    _BIS_SR(GIE); // Global Interrupt enable
-
-    TA2CTL = TASSEL_1 + MC_1 + ID_0;
-
+    TA2CTL = TASSEL_1 | MC_1 | ID_0;
     TA2CCR0 = 327;
 
-    TA2CCTL0 = CCIE; // Enable interrupt for TA2CCR0
+    TA2CCTL0 = CCIE;
 
-int loops = 0;
-    while(loops < 30) {
-        if(led == 0x0001) {
-            BuzzerOn(13,1);
-        } else {
-            BuzzerOn(13,1);
+    while (index < 10)
+    {
+        if (nextBeat)
+        {
+            nextBeat = 0;
+
+            if (song[index].duration > 0)
+            {
+                song[index].duration--;
+            }
+            else
+            {
+                index++;
+            }
+
         }
+        playNote(song[index]);
     }
-    BuzzerOff();
+}
+void playNote(struct Note note)
+{
+    BuzzerOn(note.frequency, 100);
+
+    if (note.frequency <= 494)
+    {
+        writeLED(1);
+    }
+    else if (note.frequency <= 587)
+    {
+        writeLED(2);
+    }
+    else if (note.frequency <= 698)
+    {
+        writeLED(4);
+    }
+    else
+    {
+        writeLED(8);
+    }
+}
+
+void makeSong()
+{
+
+    song[0].duration = 500;
+    song[0].frequency = 440;
+
+    song[1].duration = 500;
+    song[1].frequency = 1000;
+
+    song[3].duration = 500;
+    song[3].frequency = 440;
+
+    song[4].duration = 500;
+    song[4].frequency = 1000;
+
+    song[5].duration = 500;
+       song[5].frequency = 440;
+
+       song[6].duration = 500;
+       song[6].frequency = 1000;
+       song[7].duration = 500;
+          song[7].frequency = 440;
+
+      song[8].duration = 500;
+      song[8].frequency = 1000;
+
+     song[9].duration = 500;
+     song[9].frequency = 440;
+
 }
 
 // Example syntax for TimerA2 ISR
 #pragma vector=TIMER2_A0_VECTOR
-__interrupt void TIMER_A2_ISR(void)
+__interrupt void TIMERA2_ISR(void)
 {
-    led ^= BIT0;
+    nextBeat = 1;
 }
 
+void showLEDs(unsigned int val)
+{
+
+}
 void configureButtons()
 {
-    //buttons on P7.0, P7.4, P2.4, P3.6
+//buttons on P7.0, P7.4, P2.4, P3.6
 
-    //configure switches for IO
+//configure switches for IO
     P7SEL &= ~(BIT0 | BIT4);
     P2SEL &= ~(BIT2);
     P3SEL &= ~(BIT6);
 
-    //set them up for input
+//set them up for input
     P7DIR &= ~(BIT0 | BIT4);
     P2DIR &= ~(BIT2);
     P3DIR &= ~(BIT6);
 
-    //enable the pull resistors
+//enable the pull resistors
     P7REN |= (BIT0 | BIT4);
     P2REN |= (BIT2);
     P3REN |= (BIT6);
 
-    //specify that they should be pull resistors
-    P7OUT &= ~(BIT0 | BIT4);
-    P2OUT &= ~(BIT2);
-    P3OUT &= ~(BIT6);
+//specify that they should be pull resistors
+    P7OUT |= (BIT0 | BIT4);
+    P2OUT |= (BIT2);
+    P3OUT |= (BIT6);
 }
 
 unsigned int readButtons()
 {
-    //buttons on P7.0(S1), P7.4 (S2), P2.4 (S3), P3.7 (S4)
-    return ~(~(P7IN & (BIT0)) | ~(P3IN & (BIT6) >> 3) | ~(P2IN & (BIT2))
-            | ~(P7IN & (BIT4)));
+//buttons on P7.0(S1), P7.4 (S2), P2.4 (S3), P3.7 (S4)
+    return (~P7IN & (BIT0)) | ((~P3IN & (BIT6)) >> 5) | (~P2IN & (BIT2))
+            | ((~P7IN & (BIT4)) >> 1);
 }
-void configUserLED(char inbits)
+void configUserLED()
 {
-    P1SEL &= ~(BIT0); //set select pin to 0, digital io on
-    P4SEL &= ~(BIT7);
-    P1DIR |= (BIT0); //set direction pin to 1, output
-    P4DIR |= (BIT7);
-    //turn target off, then set to inbitsTarget
-    P1OUT = (P1OUT & ~(BIT0)) | (inbits & BIT0); //set p1.0 to inbitsBIT0
-    P4OUT = (P4OUT & ~(BIT7)) | (inbits & BIT1); //set p4.7 to inbitsBIT1
+    P6SEL &= ~(BIT2 | BIT1 | BIT3 | BIT4);
+    P6DIR |= (BIT2 | BIT1 | BIT3 | BIT4);
 }
-
-void swDelay(char numLoops)
+void writeLED(unsigned int val)
 {
-    // This function is a software delay. It performs
-    // useless loops to waste a bit of time
-    //
-    // Input: numLoops = number of delay loops to execute
-    // Output: none
-    //
-    // smj, ECE2049, 25 Aug 2013
+    P6OUT &= ~(BIT2 | BIT1 | BIT3 | BIT4);
 
-    volatile unsigned int i, j;	// volatile to prevent removal in optimization
-                                // by compiler. Functionally this is useless code
-
-    for (j = 0; j < numLoops; j++)
+    if ((val & BIT0) != 0)
     {
-        i = 1000;					// SW Delay
-        while (i > 0)				// could also have used while (i)
-            i--;
+        P6OUT |= BIT2;
+    }
+    if ((val & BIT1) != 0)
+    {
+        P6OUT |= BIT1;
+    }
+    if ((val & BIT2) != 0)
+    {
+        P6OUT |= BIT3;
+    }
+    if ((val & BIT3) != 0)
+    {
+        P6OUT |= BIT4;
     }
 }
