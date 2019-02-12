c671944c74353fa2eefd7e41050ad96fa64de0e2
emmettmccann <mccannemmett@gmail.com>
2019-02-11 21:39:27 -0500

    formatting

--- main.c
+++ main.c
@@ -27,9 +27,8 @@ void makeSong();
 // Declare globals here
 unsigned int led = 0x00;
 
-struct Note
-{
-    int frequency, duration;
+struct Note {
+  int frequency, duration;
 };
 void playNote(struct Note not);
 
@@ -38,161 +37,129 @@ struct Note song[10];
 volatile int nextBeat = 0;
 
 // Main
-void main()
-{
-   WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer. Always need to stop this!!
-   _BIS_SR(GIE);
+void main() {
+  WDTCTL = WDTPW | WDTHOLD;  // Stop watchdog timer. Always need to stop this!!
+  _BIS_SR(GIE);
 
-    configUserLED();
-    configureButtons();
-    makeSong();
+  configUserLED();
+  configureButtons();
+  makeSong();
 
-    //setup the timer here
+  // setup the timer here
+  int index = 0;
 
-    int index = 0;
+  TA2CTL = TASSEL_1 | MC_1 | ID_0;
+  TA2CCR0 = 327;
 
-    TA2CTL = TASSEL_1 | MC_1 | ID_0;
-    TA2CCR0 = 327;
+  TA2CCTL0 = CCIE;
+  //-----------------------------------------------
 
-    TA2CCTL0 = CCIE;
+  while (index < 10) {
+    if (nextBeat) {
+      nextBeat = 0;
 
-    while (index < 10)
-    {
-        if (nextBeat)
-        {
-            nextBeat = 0;
-
-            if (song[index].duration > 0)
-            {
-                song[index].duration--;
-            }
-            else
-            {
-                index++;
-            }
-
-        }
-        playNote(song[index]);
+      if (song[index].duration > 0) {
+        song[index].duration--;
+      } else {
+        index++;
+      }
     }
+    playNote(song[index]);
+  }
 }
-void playNote(struct Note note)
-{
-    BuzzerOn(note.frequency, 100);
+void playNote(struct Note note) {
+  BuzzerOn(note.frequency, 100);
 
-    if (note.frequency <= 494)
-    {
-        writeLED(1);
-    }
-    else if (note.frequency <= 587)
-    {
-        writeLED(2);
-    }
-    else if (note.frequency <= 698)
-    {
-        writeLED(4);
-    }
-    else
-    {
-        writeLED(8);
-    }
+  if (note.frequency <= 494) {
+    writeLED(1);
+  } else if (note.frequency <= 587) {
+    writeLED(2);
+  } else if (note.frequency <= 698) {
+    writeLED(4);
+  } else {
+    writeLED(8);
+  }
 }
 
-void makeSong()
-{
+void makeSong() {
+  song[0].duration = 500;
+  song[0].frequency = 440;
 
-    song[0].duration = 500;
-    song[0].frequency = 440;
+  song[1].duration = 500;
+  song[1].frequency = 1000;
 
-    song[1].duration = 500;
-    song[1].frequency = 1000;
+  song[3].duration = 500;
+  song[3].frequency = 440;
 
-    song[3].duration = 500;
-    song[3].frequency = 440;
+  song[4].duration = 500;
+  song[4].frequency = 1000;
 
-    song[4].duration = 500;
-    song[4].frequency = 1000;
+  song[5].duration = 500;
+  song[5].frequency = 440;
 
-    song[5].duration = 500;
-       song[5].frequency = 440;
+  song[6].duration = 500;
+  song[6].frequency = 1000;
+  song[7].duration = 500;
+  song[7].frequency = 440;
 
-       song[6].duration = 500;
-       song[6].frequency = 1000;
-       song[7].duration = 500;
-          song[7].frequency = 440;
+  song[8].duration = 500;
+  song[8].frequency = 1000;
 
-      song[8].duration = 500;
-      song[8].frequency = 1000;
-
-     song[9].duration = 500;
-     song[9].frequency = 440;
-
+  song[9].duration = 500;
+  song[9].frequency = 440;
 }
 
 // Example syntax for TimerA2 ISR
-#pragma vector=TIMER2_A0_VECTOR
-__interrupt void TIMERA2_ISR(void)
-{
-    nextBeat = 1;
+#pragma vector = TIMER2_A0_VECTOR
+__interrupt void TIMERA2_ISR(void) { nextBeat = 1; }
+
+void showLEDs(unsigned int val) {}
+void configureButtons() {
+  // buttons on P7.0, P7.4, P2.4, P3.6
+
+  // configure switches for IO
+  P7SEL &= ~(BIT0 | BIT4);
+  P2SEL &= ~(BIT2);
+  P3SEL &= ~(BIT6);
+
+  // set them up for input
+  P7DIR &= ~(BIT0 | BIT4);
+  P2DIR &= ~(BIT2);
+  P3DIR &= ~(BIT6);
+
+  // enable the pull resistors
+  P7REN |= (BIT0 | BIT4);
+  P2REN |= (BIT2);
+  P3REN |= (BIT6);
+
+  // specify that they should be pull resistors
+  P7OUT |= (BIT0 | BIT4);
+  P2OUT |= (BIT2);
+  P3OUT |= (BIT6);
 }
 
-void showLEDs(unsigned int val)
-{
-
+unsigned int readButtons() {
+  // buttons on P7.0(S1), P7.4 (S2), P2.4 (S3), P3.7 (S4)
+  return (~P7IN & (BIT0)) | ((~P3IN & (BIT6)) >> 5) | (~P2IN & (BIT2)) |
+         ((~P7IN & (BIT4)) >> 1);
 }
-void configureButtons()
-{
-//buttons on P7.0, P7.4, P2.4, P3.6
-
-//configure switches for IO
-    P7SEL &= ~(BIT0 | BIT4);
-    P2SEL &= ~(BIT2);
-    P3SEL &= ~(BIT6);
-
-//set them up for input
-    P7DIR &= ~(BIT0 | BIT4);
-    P2DIR &= ~(BIT2);
-    P3DIR &= ~(BIT6);
-
-//enable the pull resistors
-    P7REN |= (BIT0 | BIT4);
-    P2REN |= (BIT2);
-    P3REN |= (BIT6);
-
-//specify that they should be pull resistors
-    P7OUT |= (BIT0 | BIT4);
-    P2OUT |= (BIT2);
-    P3OUT |= (BIT6);
+void configUserLED() {
+  P6SEL &= ~(BIT2 | BIT1 | BIT3 | BIT4);
+  P6DIR |= (BIT2 | BIT1 | BIT3 | BIT4);
 }
+void writeLED(unsigned int val) {
+  P6OUT &= ~(BIT2 | BIT1 | BIT3 | BIT4);
 
-unsigned int readButtons()
-{
-//buttons on P7.0(S1), P7.4 (S2), P2.4 (S3), P3.7 (S4)
-    return (~P7IN & (BIT0)) | ((~P3IN & (BIT6)) >> 5) | (~P2IN & (BIT2))
-            | ((~P7IN & (BIT4)) >> 1);
-}
-void configUserLED()
-{
-    P6SEL &= ~(BIT2 | BIT1 | BIT3 | BIT4);
-    P6DIR |= (BIT2 | BIT1 | BIT3 | BIT4);
-}
-void writeLED(unsigned int val)
-{
-    P6OUT &= ~(BIT2 | BIT1 | BIT3 | BIT4);
-
-    if ((val & BIT0) != 0)
-    {
-        P6OUT |= BIT2;
-    }
-    if ((val & BIT1) != 0)
-    {
-        P6OUT |= BIT1;
-    }
-    if ((val & BIT2) != 0)
-    {
-        P6OUT |= BIT3;
-    }
-    if ((val & BIT3) != 0)
-    {
-        P6OUT |= BIT4;
-    }
+  if ((val & BIT0) != 0) {
+    P6OUT |= BIT2;
+  }
+  if ((val & BIT1) != 0) {
+    P6OUT |= BIT1;
+  }
+  if ((val & BIT2) != 0) {
+    P6OUT |= BIT3;
+  }
+  if ((val & BIT3) != 0) {
+    P6OUT |= BIT4;
+  }
 }
--- switches.c
+++ switches.c
@@ -22,25 +22,25 @@ char switchIO();
  * Don't affect other pins
  */
 void switchConfig() {
-    P2SEL = P2SEL & ~(BIT6 | BIT5 | BIT4 | BIT3); //1000 0111 & P2SEL
-    P2REN = P2SEL & ~(BIT6 | BIT5 | BIT4 | BIT3); //1000 0111 & P2DIR
+  P2SEL = P2SEL & ~(BIT6 | BIT5 | BIT4 | BIT3);  // 1000 0111 & P2SEL
+  P2REN = P2SEL & ~(BIT6 | BIT5 | BIT4 | BIT3);  // 1000 0111 & P2DIR
 
-    P2DIR = P2DIR & ~(BIT6 | BIT5 | BIT4 | BIT3); //1000 0111 & P2DIR (sets 6 - 3 to 0 for input, while not modifying the others)
+  P2DIR = P2DIR & ~(BIT6 | BIT5 | BIT4 | BIT3);
+  // 1000 0111 & P2DIR (sets 6 - 3 to 0 for input,
+  // while not modifying the others)
 }
 char switchIO() {
-    char inBits;
-    inBits = inBits;
+  char inBits;
+  inBits = inBits;
 
-    inBits = inBits >> 3;
-    inBits = inBits & (BIT3 | BIT2 | BIT1 | BIT0);
+  inBits = inBits >> 3;
+  inBits = inBits & (BIT3 | BIT2 | BIT1 | BIT0);
 
-    if(inBits < 9) {
-        inBits += 0x30;
-    } else {
-        inBits += (0x41 - 0xA);
-    }
+  if (inBits < 9) {
+    inBits += 0x30;
+  } else {
+    inBits += (0x41 - 0xA);
+  }
 
-    return inBits;
+  return inBits;
 }
-
-
